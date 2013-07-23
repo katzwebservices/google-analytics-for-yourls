39816af80f0ac9b21acd63f69a08e15819227ee4
Zack Katz <zack@katzwebservices.com>
2013-07-23 14:13:21 -0600

    Updated version number
    Updated to 1.0.4 for jQuery fix, updated readme.

--- plugin.php
+++ plugin.php
@@ -3,7 +3,7 @@
 Plugin Name: Google Analytics
 Plugin URI: http://www.seodenver.com/yourls-analytics/
 Description: Easily add Google Analytics tracking tags to your generated links.
-Version: 1.0.3
+Version: 1.0.4
 Author: Katz Web Services, Inc.
 Author URI: http://www.katzwebservices.com
 Settings: <a href="?page=google_analytics">Configure settings</a>
@@ -24,7 +24,7 @@ function kws_yourls_analytics_defaults() {
 	$utm_medium = 'urlshortener';
 	$utm_term = $utm_content = '';
 	$utm_source = preg_replace('/(?:.*?)\:\/\/(.+)/ism', '$1', YOURLS_SITE); // http://exam.pl becomes exam.pl
-	
+
 	$query = array(
 		'utm_source' => $utm_source,
 		'utm_medium' => $utm_medium,
@@ -32,13 +32,13 @@ function kws_yourls_analytics_defaults() {
 		'utm_term' => $utm_term,
 		'utm_content' => $utm_content
 	);
-	
+
 	$defaults = yourls_get_option('analytics_defaults');
-	
+
 	if(!is_array($defaults)) { return $query; }
-	
+
 	$settings = array_merge($query, $defaults);
-	
+
 	// These two are required.
 	if(empty($settings['utm_source'])) {
 		$settings['utm_source'] = $utm_source;
@@ -46,7 +46,7 @@ function kws_yourls_analytics_defaults() {
 	if(empty($settings['utm_medium'])) {
 		$settings['utm_medium'] = $utm_medium;
 	}
-	
+
 	return $settings;
 }
 
@@ -61,14 +61,14 @@ function kws_yourls_add_analytics_do_page() {
 	$analytics_override = yourls_get_option( 'analytics_override' );
 	$add_to_form = yourls_get_option( 'add_to_form' );
 	if($add_to_form === false) { $add_to_form = 'yes'; } // Defaults to yes
-	
+
 	$analytics_defaults = (array)kws_yourls_analytics_defaults();
 
 ?>
 	<style type="text/css">
 		 .description { color:#555; font-style:italic; }
 		 #ga_settings { font-size: 120%; }
-		 #ga_settings h4 { 
+		 #ga_settings h4 {
 			margin-bottom: .25em;
 		 }
 		 .submit {
@@ -80,7 +80,7 @@ function kws_yourls_add_analytics_do_page() {
 		 .submit input {
 			font-size: 14px!important;
 		 }
-		 #more_tracking_code_info { 
+		 #more_tracking_code_info {
 			display: none;
 			padding:5px 15px 5px 15px;
 			background: #fcfcfc;
@@ -93,12 +93,12 @@ function kws_yourls_add_analytics_do_page() {
 		 .req { color: red; }
 	</style>
 	<h2>Google Analytics Settings</h2>
-	
+
 	<form method="post" id="ga_settings">
 		<h4>Tracking Tags</h4>
 		<p class="description">
 			The settings defined here will be the default Google Analytics tracking script settings. With the current configuration, the following string will be added to shortened URLs:<br/>
-			<code>?<?php 
+			<code>?<?php
 				$analytics_defaultsexample = $analytics_defaults;
 				$urlstring = array();
 				foreach($analytics_defaultsexample as $key => $value) {
@@ -121,20 +121,20 @@ function kws_yourls_add_analytics_do_page() {
 		</p>
 		<p class="description" style="text-align:center;"><span class="req">*</span> = required fields</p>
 		<?php kws_yourls_show_analytics_help(); ?>
-		
+
 		<h4>Override existing tracking tags?</h4>
-		<p class="description">If an URL already has Google Analytics tracking tags, do you want to use those instead of your default tracking tags?</p>    
+		<p class="description">If an URL already has Google Analytics tracking tags, do you want to use those instead of your default tracking tags?</p>
 		<p><label for="analytics_override_yes">Override with Defaults <input type="radio" id="analytics_override_yes" name="analytics_override" value="1"<?php if($analytics_override) { echo ' checked="checked"';}?> /></label>
 		<label for="analytics_override_no" style="padding-left:.5em; border-left:1px solid #ccc; margin-left:.5em;">Use Existing (default)<input id="analytics_override_no" type="radio" name="analytics_override" value="0"<?php if(!$analytics_override) { echo ' checked="checked"';}?> /></label></p>
 
-		
+
 		<h4>Add GA tracking options to Add URL form?</h4>
 		 <p><input type="hidden" name="add_to_form" value="no" /><label for="add_to_form_yes">Add tracking code options to Add URL form <input type="checkbox" id="add_to_form_yes" name="add_to_form" value="yes"<?php if($add_to_form != 'no') { echo ' checked="checked"';}?> /></label></p>
-					
+
 		<div class="submit">
 			<input style="display:block;" type="submit" value="Update Settings">
 		</div>
-		
+
 	</form>
 <?php
 }
@@ -142,50 +142,50 @@ function kws_yourls_add_analytics_do_page() {
 function kws_yourls_show_analytics_help() {
 ?>
 	<p style="text-align:center;">Not sure what these settings are? <a class="toggle" href="#more_tracking_code_info">Show descriptions of these terms</a></p>
-		<div id="more_tracking_code_info" class="toggle">	
+		<div id="more_tracking_code_info" class="toggle">
 			<table class="outline2" border="0" cellpadding="0" cellspacing="5">
 				<tbody>
 					<tr>
 						<td width="201">
 							<p><label for="utm_source" class="borderbottom">Source (utm_source)</label></p>
 						</td>
-		
+
 						<td width="666"><span class="req">Required.</span> Use <strong>utm_source</strong> to identify a search engine, newsletter name, or other source.<br>
 						<em>Example</em>: <tt>utm_source=google</tt></td>
 					</tr>
-		
+
 					<tr>
 						<td>
 							<p><label for="utm_medium" class="borderbottom">Medium (utm_medium)</label></p>
 						</td>
-		
+
 						<td><span class="req">Required.</span> Use <strong>utm_medium</strong> to identify a medium such as email or cost-per-click.<br>
 						<em>Example</em>: <tt>utm_medium=cpc</tt></td>
 					</tr>
-					
+
 					<tr>
 						<td>
 							<p><label for="utm_campaign" class="borderbottom">Campaign Name (utm_campaign)</label></p>
 						</td>
-		
+
 						<td>Used for keyword analysis. Use <strong>utm_campaign</strong> to identify a specific product promotion or strategic campaign.<br>
 						<em>Example</em>: <tt>utm_campaign=spring_sale</tt></td>
 					</tr>
-					
+
 					<tr>
 						<td>
 							<p><label for="utm_term" class="borderbottom">Term (utm_term)</label></p>
 						</td>
-		
+
 						<td>Used for paid search. Use <strong>utm_term</strong> to note the keywords for this ad.<br>
 						<em>Example</em>: <tt>utm_term=running+shoes</tt></td>
 					</tr>
-		
+
 					<tr>
 						<td>
 							<p><label for="utm_content" class="borderbottom">Content (utm_content)</label></p>
 						</td>
-		
+
 						<td>Used for A/B testing and content-targeted ads. Use <strong>utm_content</strong> to differentiate ads or links that point to the same URL.<br>
 						<em>Examples</em>: <tt>utm_content=logolink</tt> <em>or</em> <tt>utm_content=textlink</tt></td>
 					</tr>
@@ -219,19 +219,19 @@ yourls_add_filter( 'custom_url', 'kws_yourls_custom_url', 999);
 
 function kws_yourls_custom_url($url = '') {
 	if(!isset($_GET['allfields']) || !isset($_GET['keyword'])) { return $url; }
-	
+
 	$allfields = yourls_maybe_unserialize($_GET['allfields']);
 
 	parse_str($allfields, $fields);
-	
+
 	$accepted = array('utm_source','utm_medium','utm_campaign','utm_term','utm_content');
-	
+
 	foreach($fields as $key => $field) {
 		if(!in_array($key, $accepted) || empty($field)) {
 			unset($fields[$key]);
 		}
 	}
-	
+
 	return yourls_add_query_arg($fields, $url);
 }
 
@@ -260,27 +260,27 @@ yourls_add_action( 'admin_init', 'kws_yourls_admin_init' );
 function kws_yourls_admin_init() {
 	if((empty($_REQUEST['action']) || empty($_REQUEST['url'])) && !isset($_REQUEST['u'])) { return; }
 
-	
+
 	// Bookmark
 	if(isset($_REQUEST['u'])) {
-		
+
 		$query = kws_yourls_analytics_defaults();
-		
+
 		if(isset($_GET)) {
 			unset($_GET['u'], $_GET['t'], $_GET['k'], $_GET['s'], $_GET['signature']);
 			$queryGET = kws_yourls_process_array($_GET);
 			foreach($queryGET as $key => $qg) { if(empty($qg)) { unset($queryGET[$key]); }}
 			$query = array_merge($query, $queryGET);
-			foreach($query as $k => $q) { 
+			foreach($query as $k => $q) {
 				if(empty($q)) { unset($query[$k]); }
 			}
 		}
-		
+
 		$_GET['u'] = $_REQUEST['u'] = yourls_add_query_arg($query, $_REQUEST['u']);
-		
+
 		return;
 	}
-	
+
 	// In the admin. We only want to process on adds and edits.
 	switch( $_REQUEST['action'] ) {
 		case 'add':
@@ -339,7 +339,7 @@ function add() {
 }
 
 	jQuery(document).ready(function($){
-		$('a.toggle').live('load click', function(e) {
+		$('a.toggle').on('load click', function(e) {
 			e.preventDefault();
 			var $div = $($(this).attr('href'));
 			var $that = $(this);
@@ -353,30 +353,30 @@ function add() {
 			return false;
 		});
 		$('#new_url_form').append('<a style="display:block" class="toggle" href="#ga_tracking_options">Show GA Tracking Options</a><div class="toggle" id="ga_tracking_options" style="display:none;"><?php
-	
+
 	$sources = kws_yourls_analytics_defaults();
-	
+
 	foreach($sources as $source => $value) {
 		$sourceNice = preg_replace('/(?:.*?)\_(.+)/ism', '$1', $source);
 		echo '<label for="'.$source.'" style="padding-left:.75em;">'.$sourceNice.' <input id="'.$source.'" type="text" size="12" name="'.$source.'" value="'.$value.'" /></label>';
 	}
-		
-		
+
+
 	?></div><div style="clear:both;"></div>');
-	
+
 	});
-	
+
 </script>
 <?php
 }
 
-yourls_add_filter( 'get_keyword_info', 'kws_yourls_add_analytics_tracking_code', 999, 4); 
+yourls_add_filter( 'get_keyword_info', 'kws_yourls_add_analytics_tracking_code', 999, 4);
 
 function kws_yourls_add_analytics_tracking_code($return, $keyword, $field, $notfound = false) {
-	
+
 	// If we're not working with a long URL, this filter is unnecessary.
 	if($field !== 'url' || yourls_is_admin() || defined('YOURLS_INFOS') || defined('YOURLS_PREVIEW')) { return $return; }
-	
+
 	// If we are working with a long URL, then let's get to it!
 	$url = $return;
 
@@ -384,23 +384,23 @@ function kws_yourls_add_analytics_tracking_code($return, $keyword, $field, $notf
 	if (empty($url)) {
 		return $return;
 	}
-	
+
 	$parsed = parse_url($url);
 		$parsed['scheme'] = isset($parsed['scheme']) ? $parsed['scheme'] : 'http';
 		$parsed['host'] = isset($parsed['host']) ? $parsed['host'] : '';
 		$parsed['path'] = isset($parsed['path']) ? $parsed['path'] : '';
-	
+
 	$urlStripped = $parsed['scheme'].'://'.$parsed['host'].$parsed['path'];
 	$urlQueryString = array();
 	$query = kws_yourls_analytics_defaults();
 
 	$urlParsed = parse_url($url);
-	
+
 	// Are there query args in the long URL? We'll want an array of those, thanks.
 	if(isset($urlParsed['query'])) {
 		parse_str($urlParsed['query'], $urlQueryString);
 	}
-	
+
 	// Override trumps embedded long URL query args
 	if(yourls_get_option('analytics_override') == 1) {
 		echo '<h2>After override</h2>';
@@ -416,27 +416,27 @@ function kws_yourls_add_analytics_tracking_code($return, $keyword, $field, $notf
 		if(isset($_GET['u'])) {
 			unset($_GET['u'], $_GET['t'], $_GET['k'], $_GET['signature']);
 		}
-		foreach($_GET as $key => $value) { 
+		foreach($_GET as $key => $value) {
 			if(!empty($value)) { $query[$key] = $value; }
 		}
 	}
-	
+
 	// Add back in the query string
 	$url = yourls_add_query_arg($query, $urlStripped);
-	
+
 	return $url;
 }
 
 function kws_yourls_parse_url($url) {
 
 	$urlParsed = parse_url($url);
-	
+
 	if(isset($urlParsed['query'])) {
 		parse_str($urlParsed['query'], $urlParsed);
 	}
-	
+
 	if(empty($urlParsed)) { return $url; }
-	
+
 	foreach($urlParsed as $key => $queryString) {
 		$queryStringParsed = explode('=', $queryString);
 		unset($urlParsed[$key]);
@@ -446,7 +446,7 @@ function kws_yourls_parse_url($url) {
 			continue;
 		}
 	}
-	
+
 	return $urlParsed;
 }
 
@@ -469,10 +469,10 @@ function kws_yourls_process_source_abbr($value = '') {
 }
 
 function kws_yourls_process_array($query) {
-	
+
 	if(!is_array($query)) { return $query; }
-	
-	
+
+
 	foreach($query as $key => $value) {
 		switch($key) {
 			case 'utm_medium':
@@ -528,15 +528,15 @@ function kws_yourls_process_array($query) {
 				break;
 		}
 	}
-	
+
 	unset($query['utm_source'], $query['utm_medium'], $query['utm_campaign'], $query['utm_term'], $query['utm_content']);
-		
+
 	$query['utm_source'] = $utm_source;
 	$query['utm_medium'] = $utm_medium;
 	$query['utm_campaign'] = $utm_campaign;
 	$query['utm_term'] = $utm_term;
 	$query['utm_content'] = $utm_content;
-	
+
 	return $query;
 
 }
